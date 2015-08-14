b0f8a79f012b76d676713f70c16be095c5ed1823
Liang <liang@unchartedworks.com>
2015-08-14 14:49:08 +0200

    Initial Commit

---
+++ HaskellSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HaskellSwift
+//
+//  Created by Liang on 14/08/2015.
+//  Copyright © 2015 Liang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HaskellSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HaskellSwift
+//
+//  Created by Liang on 14/08/2015.
+//  Copyright © 2015 Liang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
