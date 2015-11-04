db8185e2a791afbbb155b88372b2eff51941f138
Liang <liang@X99.local>
2015-11-04 12:32:00 +0100

    Adds SwiftCheck

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /Users/liang/Projects/HaskellSwift
-while true
-do
-  xcodebuild  -project HaskellSwift.xcodeproj -scheme HaskellSwiftTests test 
-  sleep 10
-done
+cd /Users/liang/Dropbox/Workspace/HaskellSwift
+#while true
+#do
+  xcodebuild -project HaskellSwift.xcodeproj -scheme HaskellSwift build
+  xctool  -project HaskellSwift.xcodeproj -scheme HaskellSwift run-tests -parallelize
+#  sleep 10
+#done
