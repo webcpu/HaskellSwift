6b8a0f7acda61490f111861477c483c06d69ffeb
Liang <liang@X99.local>
2015-11-06 22:28:24 +0100

    Fixes a bug of the build script

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 cd /Users/liang/Dropbox/Workspace/HaskellSwift
 #while true
 #do
-  xcodebuild -project HaskellSwift.xcodeproj -scheme HaskellSwift build
+if xcodebuild -project HaskellSwift.xcodeproj -scheme HaskellSwift build ; then
   xctool  -project HaskellSwift.xcodeproj -scheme HaskellSwift run-tests -parallelize
+fi
 #  sleep 10
 #done
