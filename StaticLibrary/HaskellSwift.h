83dbbbfd38d9b722c491f8dbf43827e782c0332b
unchartedworks <liang@unchartedworks.com>
2017-10-31 13:40:00 +0100

    Feature: add static library for macOS

---
+++ StaticLibrary/HaskellSwift.h
@@ -0,0 +1,13 @@
+//
+//  HaskellSwift.h
+//  HaskellSwift
+//
+//  Created by Liang on 30/10/2017.
+//  Copyright © 2017 Liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HaskellSwift : NSObject
+
+@end
---
+++ StaticLibrary/HaskellSwift.m
@@ -0,0 +1,13 @@
+//
+//  HaskellSwift.m
+//  HaskellSwift
+//
+//  Created by Liang on 30/10/2017.
+//  Copyright © 2017 Liang. All rights reserved.
+//
+
+#import "HaskellSwift.h"
+
+@implementation HaskellSwift
+
+@end
