45d94c19aba8cd1e4d665dd0e4f21dd99b008531
unchartedworks <liang@unchartedworks.com>
2019-11-13 14:49:14 +0100

    Feature: Supports Mac Catalyst

---
+++ HaskellSwift-macOSCatalyst/HaskellSwift_macOSCatalyst.h
@@ -0,0 +1,19 @@
+//
+//  HaskellSwift_macOSCatalyst.h
+//  HaskellSwift-macOSCatalyst
+//
+//  Created by L Y on 2019-11-04.
+//  Copyright © 2019 Liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HaskellSwift_macOSCatalyst.
+FOUNDATION_EXPORT double HaskellSwift_macOSCatalystVersionNumber;
+
+//! Project version string for HaskellSwift_macOSCatalyst.
+FOUNDATION_EXPORT const unsigned char HaskellSwift_macOSCatalystVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HaskellSwift_macOSCatalyst/PublicHeader.h>
+
+
